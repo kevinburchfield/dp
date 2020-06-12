d38daa1a1237f4a6c4fe5adfa1160a8f158531b6
Kevin <kevinburchfi@gmail.com>
2020-06-12 01:04:18 -0400

    finished

--- rust/necklace_matching/neckace_matching.rs
+++ rust/necklace_matching/neckace_matching.rs
@@ -2,11 +2,16 @@ use std::io;
 use std::io::BufRead;
 use std::fs::File;
 use std::env;
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let test_to_run = &args[2];
+
+    let mut word_map: HashMap<String, Vec<String>> = HashMap::new();
+    let start = Instant::now();
     
     if let Ok(lines) = open_and_read(filename) {
         for line in lines {
@@ -17,7 +22,13 @@ fn main() -> io::Result<()> {
                         println!("Necklace \"{}\" repeats {} times", necklace, repeats(split[0]));
                     }
                     "bonus_2" => {
-                        println!("Not implemented yet.");
+                        if necklace.len() >= 4 {
+                            let rotated: String = rotate_to_sort(split[0]);
+                            if word_map.get(&(rotated.clone())).is_none() {
+                                word_map.insert(rotated.clone(), Vec::new());
+                            } 
+                            word_map.get_mut(&(rotated.clone())).unwrap().push(String::from(split[0]));
+                        }
                     },
                     _ => {
                         println!("Necklace: {} | Test: {} | Result: {}", split[0], split[1], does_necklace_match((split[0], split[1])));
@@ -28,6 +39,16 @@ fn main() -> io::Result<()> {
         }
     }
 
+    if test_to_run == "bonus_2" {
+        for (key, val) in word_map.iter() {
+            if (val.len() as i32) == 4 {
+                println!("[{}]", val.iter().fold(String::new(), |acc, st| acc + &st.to_string() + ", "));
+            }
+        }
+    }
+
+    let duration = start.elapsed();
+    println!("Time elapse: {:?}", duration);
     Ok(())
 }
 
@@ -54,6 +75,7 @@ fn repeats(necklace: &str) -> i32 {
         let back = String::from(necklace.get(..i+1).unwrap());
         let front = String::from(necklace.get(i+1..).unwrap());
         test_string = front + &back;
+        println!("{}", test_string);
         if test_string == necklace {
             matches+=1;
         }
@@ -61,6 +83,23 @@ fn repeats(necklace: &str) -> i32 {
     return matches;
 }
 
-fn enable_search() {
-    
+fn rotate_to_sort(necklace: &str) -> String {
+    let mut best_string: Option<String> = Some(String::from(""));
+    let mut best_char: char = 'z';
+    let mut test_string: String;
+    for i in 0..necklace.len() {
+        let back = String::from(necklace.get(..i+1).unwrap());
+        let front = String::from(necklace.get(i+1..).unwrap());
+        test_string = front + &back;
+
+        let vec: Vec<char> = test_string.chars().collect();
+        if (vec[0] as i32) < (best_char as i32) {
+            match best_string.as_mut() {
+                Some(v) => *v = test_string,
+                None => {},
+            }
+            best_char = vec[0];
+        }
+    }
+    return best_string.unwrap();
 }
